da5f01380ad7bf3e3130f0134ec5f41af92d7ae1
webtracer <74070641+webtracer@users.noreply.github.com>
2022-02-08 08:55:43 -0800

    Add files via upload

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// Exanple
+            /// public int Add(int num1, int num2)
+            /// {
+            ///     int result = num1 + num2;
+            ///     return result;
+            ///  }    
+            ///   Can also be done as return num1 + num2
+
+            WriteSomething();
+            WriteSomethingSpecific("I am an argument and am called from a method");
+            Console.WriteLine(Add(15, 31)); // can use the method call as an argument
+            Console.WriteLine(Add(Add(2,1),Add(4,3)));
+            Console.WriteLine(Multiplier(9,9));
+            Console.WriteLine(Division(144,12));
+            Console.Read();
+        }
+
+        public static void WriteSomething()
+        {
+            // Must be static to be called from static Method like Main
+            Console.WriteLine("I an called from a method");
+            //Console.Read();
+        }
+
+        public static void WriteSomethingSpecific(string myText)
+        {
+            Console.WriteLine(myText);
+            //Console.Read();
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Multiplier(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double Division(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ Methods_Challenge/Methods_Challenge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Methods_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string friend1 = "Laura";
+            string friend2 = "Stew";
+            string friend3 = "Kosta";
+
+            Console.WriteLine(GreetFriend(friend1));
+            Console.WriteLine(GreetFriend(friend2));
+            Console.WriteLine(GreetFriend(friend3));
+        }
+
+        static string GreetFriend(string friendsName)
+        {
+            return $"Hi {friendsName}, my friend!";
+        }
+
+    }
+}
